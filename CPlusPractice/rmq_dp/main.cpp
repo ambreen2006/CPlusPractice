068db5427bbda200ec1008d9b639ac66eb94e290
Ambreen Haleem <ahaleem@pertino.com>
2017-02-03 01:10:12 -0800

    Range sum query using dynamic programming

---
+++ CPlusPractice/rmq_dp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rmq_dp
+//
+//  Created by ahaleem on 2/2/17.
+//  Copyright © 2017 Ambreen Haleem. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
