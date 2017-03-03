19ce81e7e2fab59314daf222ed73d7d23878ded8
Ambreen Haleem <ahaleem@pertino.com>
2017-03-02 16:27:13 -0800

    range min query, with DP

--- CPlusPractice/rmq_dp/main.cpp
+++ CPlusPractice/rmq_dp/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+<<<<<<< HEAD
 
 using namespace std;
 
@@ -23,15 +24,83 @@ int rmq(int i, int j, vector<int>& array) {
     
     return result;
 }
+=======
+#include <numeric>
+
+using namespace std;
+
+#define MAX_N 1000
+#define LOG_TWO_N 10
+
+class RMQ {
+private:
+	int _A[MAX_N], SpT[MAX_N][LOG_TWO_N];
+public:
+	RMQ(int n, int A[]) {
+		for (int i = 0; i < n; i++) {
+			_A[i] = A[i];
+			SpT[i][0] = i;
+		}
+		
+		for(int j = 1; (1 << j) <=n ; j++) {
+			for(int i = 0; i + ((1<<j)-1) < n;i++) {
+				if(_A[SpT[i][j-1]] < _A[SpT[i+(1 << (j-1))][j-1]])
+				{
+					SpT[i][j] = SpT[i][j-1];
+				}
+				else {
+					SpT[i][j] = SpT[i+(1<<(j-1))][j-1];
+				}
+			}
+		}
+		
+	} // end of rmq
+	
+	int query(int i, int j) {
+		int k = (int) floor(log((double)j-i+1)/log(2.0));
+		if(_A[SpT[i][k]] <= _A[SpT[j-(1<<k)+1][k]])
+			return SpT[i][k];
+		else
+			return SpT[j-(1<<k)+1][k];
+	}
+};
+
+class RangeSumDP {
+public:
+	RangeSumDP(const vector<int> _arr) {
+		arr = _arr;
+		spt.assign(arr.size()+1, 0);
+		partial_sum(arr.begin(), arr.end(), spt.begin()+1);
+		copy(spt.begin(),spt.end(),ostream_iterator<int>(cout, ","));
+	}
+	
+	int rsq(int i, int j) {
+		return (spt[j+1] - spt[i]);
+	}
+	
+private:
+	vector<int> spt, arr;
+};
+>>>>>>> range min query, with DP
 
 int main(int argc, const char * argv[]) {
 	// insert code here...
 	std::cout << "Hello, World!\n";
+<<<<<<< HEAD
     
     int N = 16;
     vector<int> array {2,4,3,1,6,7};
     rmq(1, 3, array);
     
     cout << endl;
+=======
+	vector<int> arr = {18, 17, 13, 19, 15};
+	copy(arr.begin(),arr.end(),ostream_iterator<int>(cout,","));
+	cout << endl;
+	RangeSumDP dp(arr);
+	
+	cout << endl;
+	cout << "RMQ(0,2) = " << dp.rsq(0,0) << endl;
+>>>>>>> range min query, with DP
     return 0;
 }
